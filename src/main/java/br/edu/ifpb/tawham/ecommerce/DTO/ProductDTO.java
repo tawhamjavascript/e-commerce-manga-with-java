849c7c2d340dd29f32c23cb0216b10e892f632d3
tawhamjavascript <taw.ham.balbino9914@gmail.com>
2023-07-06 16:01:10 -0300

    code finished

--- src/main/java/br/edu/ifpb/tawham/ecommerce/DTO/ProductDTO.java
+++ src/main/java/br/edu/ifpb/tawham/ecommerce/DTO/ProductDTO.java
@@ -6,16 +6,17 @@ import br.edu.ifpb.tawham.ecommerce.model.Product;
 import jakarta.validation.constraints.NegativeOrZero;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record ProductDTO(
     Long id, 
     @NotBlank(message = "name cannot be empty") @NotNull(message = "name is necessary to register product") String name,
-    @NegativeOrZero(message = "Chapter need be bigger than zero") int chapter,
+    @Positive(message = "Chapter need be bigger than zero") int chapter,
     @NotBlank(message = "description cannot be empty") @NotNull(message = "description is necessary to register product") String description,
     @NotBlank(message = "genre cannot be empty") @NotNull(message = "genre is necessary to register product")String genre,
     @NotBlank(message = "author cannot be empty") @NotNull(message = "author is necessary to register product")String author,
     @NotBlank(message = "publication cannot be empty") @NotNull(message = "publication is necessary to register product")String publication,
-    @NegativeOrZero(message = "price need be bigger than zero") double price,
+    @Positive(message = "price need be bigger than zero") double price,
     @URL String image) {
     public ProductDTO(Product product) {
         this(product.getId(), product.getName(), product.getChapter(), 
